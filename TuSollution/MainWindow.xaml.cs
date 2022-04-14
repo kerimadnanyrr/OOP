8eae37c675a090dec372bd2e08da096a7174a433
Sabahci <Sabahci@DESKTOP-G3HIRR7>
2022-04-14 09:33:36 +0300

    Add project files.

---
+++ TuSollution/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using TuSollution.Entities;
+using WpfRehber;
+using WpfRehber.Repositories;
+
+namespace TuSollution
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PersonRepositoryJson _rep;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            RefreshGrid();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddPersonForm frmAddPerson = new AddPersonForm();
+            frmAddPerson.ShowDialog();
+
+            RefreshGrid();
+        }
+
+        private void RefreshGrid()
+        {
+            _rep = new PersonRepositoryJson();
+            peopleGrid.ItemsSource = _rep.List();
+        }
+
+        private void CommandBinding_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+             var silinecek = (Person)peopleGrid.SelectedItem;
+            _rep.Delete(silinecek);
+            _rep.SaveChanges();
+            RefreshGrid();
+        }
+    }
+}
---
+++ TuSollution/Repositories/PersonRepositoryJson.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TuSollution.Entities;
+
+namespace WpfRehber.Repositories
+{
+    public class PersonRepositoryJson
+    {
+        static List<Person> _people=new List<Person>();
+
+        public void AddOrUpdate(Person person)
+        {
+            _people.Add(person);
+        }
+        public void Delete(Person person)
+        {
+            _people.Remove(person);
+        }
+
+        public List<Person> List()
+        {
+            string fileContent = File.ReadAllText("Kişiler.json");
+            _people = JsonSerializer.Deserialize<List<Person>>(fileContent);
+            return _people.ToList();
+        }
+
+        public void SaveChanges() 
+        {
+            string serializedPeople=JsonSerializer.Serialize(_people);
+            File.WriteAllText("Kişiler.json", serializedPeople);
+
+        }
+
+    }
+}
